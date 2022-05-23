8f5ac693260eecd2a497420fd71f1421b165da02
Rodrigomeckel <85050720+Rodrigomeckel@users.noreply.github.com>
2022-05-23 16:42:25 -0300

    Projeto_Física

---
+++ ConsoleApplication18/Program.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication18
+{
+    class Program
+    {
+        public static string titulo()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                             ╔╦╗╦═╗╔═╗╔╗ ╔═╗╦  ╦ ╦╔═╗    ╔╦╗╔═╗    ╔═╗╦╔═╗╦╔═╗╔═╗");
+            Console.WriteLine("                              ║ ╠╦╝╠═╣╠╩╗╠═╣║  ╠═╣║ ║     ║║║╣     ╠╣ ║╚═╗║║  ╠═╣");
+            Console.WriteLine("                              ╩ ╩╚═╩ ╩╚═╝╩ ╩╩═╝╩ ╩╚═╝    ═╩╝╚═╝    ╚  ╩╚═╝╩╚═╝╩ ╩");
+            Console.ResetColor();
+            Console.WriteLine("");
+            return null;
+        }
+        public static string introducao()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                      Bom dia! O nosso grupo decidiu apresentar um trabalho com a junção ");
+            Console.WriteLine("                      literal de física e informática. A física está presente em diversas");
+            Console.WriteLine("                      do nosso cotidiano, desde uma vela se apagando até a batida situações");
+            Console.WriteLine("                      situações do nosso cotidiano, desde uma vela se apagando até a batida de um carro.");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("< Aperte ENTER para continuar >");
+            Console.ReadKey();
+            Console.Clear();
+
+            titulo();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                      Bom dia! O nosso grupo decidiu apresentar um trabalho com a junção ");
+            Console.WriteLine("                      literal de física e informática. A física está presente em diversas");
+            Console.WriteLine("                      do nosso cotidiano, desde uma vela se apagando até a batida situações");
+            Console.WriteLine("                      situações do nosso cotidiano, desde uma vela se apagando até a batida de um carro.");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("                      O que isso tem a ver com o trabalho? Bem, nós estamos transmitindo a ");
+            Console.WriteLine("                      apresentação em forma de programação aprendida nas aulas de lógica,");
+            Console.WriteLine("                      abaixo estarão alguns exemplos da presença da física na nossa ");
+            Console.WriteLine("                                   vida e aexplicação do porque ela ocorre.");
+            Console.WriteLine("");
+            return null;
+        }
+        public static string experiencia1()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                                        A SECAGEM MISTERIOSA DAS ROUPAS:                           ");
+            Console.WriteLine("                                                (EXPERÊNCIA 1)");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                        Na semana retrasada estava muito frio, mas pouca ventania circulava pela varanda");
+            Console.WriteLine("                          de uma certa casa. As roupas demoraram dias para secar, porém semana passada foi");
+            Console.WriteLine("                           necessário apenas algumas horas para a secagem, o que explicaria isso?");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("< Aperte ENTER para continuar >");
+            Console.ReadKey();
+            Console.Clear();
+
+            titulo();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                                        A SECAGEM MISTERIOSA DAS ROUPAS:                           ");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                        Na semana retrasada estava muito frio, mas pouca ventania circulava pela varanda");
+            Console.WriteLine("                          de uma certa casa. As roupas demoraram dias para secar, porém semana passada foi");
+            Console.WriteLine("                           necessário apenas algumas horas para a secagem, o que explicaria isso?");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                        É normal as roupas secarem mais rápido com a presença de vento, elas secam porque");
+            Console.WriteLine("                        a água presa entre as fibras dos tecidos passam do estado líquido para o gasoso. ");
+            Console.WriteLine("                        A velocidade do ar sobre a superfície, ou seja o vento, produz uma menor pressão hidrodinâmica.");
+            Console.WriteLine("                        E quanto menor a pressão, mais rápida é a  mudança de um estado para outro. ");
+            Console.WriteLine("                        Por isso que as roupas secaram mais rápido semana passada do que retrasada.");
+            Console.WriteLine("");
+            return null;
+        }
+        public static string experiencia2()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                                        O ENDURECIMENTO RÁPIDO DO PÃO:                               ");
+            Console.WriteLine("                                                (EXPERÊNCIA 2)");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                        Existem duas padarias em uma mesma rua, uma costuma deixar seus pães expostos para que os clientes   ");
+            Console.WriteLine("                        consigam admirar  enquanto a outra os enrola em plástico antes da exposição. As pessoas que compraram");
+            Console.WriteLine("                        na primeira padaria teve os pães duros no dia seguinte, já a da segunda durou mais alguns dias. Não é");
+            Console.WriteLine("                        tudo pão? Porque a diferença de tempo? ");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("< Aperte ENTER para continuar >");
+            Console.ReadKey();
+            Console.Clear();
+
+            titulo();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                                        O ENDURECIMENTO RÁPIDO DO PÃO:                               ");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                        Existem duas padarias em uma mesma rua, uma costuma deixar seus pães expostos para que os clientes   ");
+            Console.WriteLine("                        consigam admirar  enquanto a outra os enrola em plástico antes da exposição. As pessoas que compraram");
+            Console.WriteLine("                        na primeira padaria teve os pães duros no dia seguinte, já a da segunda durou mais alguns dias. Não é");
+            Console.WriteLine("                        tudo pão? Porque a diferença de tempo? ");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                        Isso acontece, porque a maciez do alimento está diretamente relacionada à quantidade de água presente");
+            Console.WriteLine("                        em seu interior.O plástico é impermeável. Portanto, ele impede a saída de água do pão por evaporação.");
+            Console.WriteLine("                        Sem o plástico, a água evapora e, consequentemente, o pão fica duro.");
+            Console.WriteLine("");
+            return null;
+        }
+        public static string experiencia3()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                                        A CHAMA QUE NÃO VIRA:                               ");
+            Console.WriteLine("                                           (EXPERÊNCIA 3)");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                        Durante a madrugada faltou luz no bairro inteiro e os moradores decidiram acender velas. Uma criança estava");
+            Console.WriteLine("                        brincando com uma das velas na mão, a chama é normalmente para cima, mas um fato curioso é que quando a vela");
+            Console.WriteLine("                        foi virada de cabeça para baixo, continuou com a chama para o alto. Não era para estar para baixo?");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("< Aperte ENTER para continuar >");
+            Console.ReadKey();
+            Console.Clear();
+
+            titulo();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                                        A CHAMA QUE NÃO VIRA:                               ");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                        Durante a madrugada faltou luz no bairro inteiro e os moradores decidiram acender velas. Uma criança estava");
+            Console.WriteLine("                        brincando com uma das velas na mão, a chama é normalmente para cima, mas um fato curioso é que quando a vela");
+            Console.WriteLine("                        foi virada de cabeça para baixo, continuou com a chama para o alto. Não era para estar para baixo?");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                        Os gases expelidos pelo pavio da vela ficam muito quentes e, por isso, acabam subindo. Isso faz parte de um");
+            Console.WriteLine("                        fenômeno físico chamado convecção, que ocorre em líquidos e gases. Se a vela for colocada de cabeça para baixo,");
+            Console.WriteLine("                        a convecção continua acontecendo fazendo com que os gases quentes subam e a chama fique para cima.");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                        Com essas três pequenas situações já deu para se notar que a física está em todo lugar, aposto que um desses");
+            Console.WriteLine("                        casos já deve ter acontecido com vocês e nem se deu conta ou se interessou pelo motivo. Agora para finalizar");
+            Console.WriteLine("                        nosso trabalho decidimos dar exemplos de dispositivos tecnológicos que auxiliam o estudo da física.");
+            Console.WriteLine("");
+            return null;
+        }
+        public static string curiosidade()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                                        SMARTPHONES E TABLETS:                               ");
+            Console.WriteLine("                                           (CURIOSIDADE)");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                        Os smartphones e tablets são como computadores. Extremamente portáteis, eles utilizam o princípio da propagação");
+            Console.WriteLine("                        de ondas, especificamente as micro-ondas, e suas aplicações vão muito além da comunicação telefônica ou acesso à internet.");
+            Console.WriteLine("                        Esses aparelhos podem ser muito úteis para entender a física no nosso dia a dia, pois trazem uma variedade de ");
+            Console.WriteLine("                        sensores que medem grandezas físicas como o magnetômetro (campo magnético), luxímetro (intensidade luminosa), ");
+            Console.WriteLine("                        barômetro (pressão atmosférica), higrômetro (umidade do ar), GPS (posição) e sensor de proximidade, além dos ");
+            Console.WriteLine("                        facilmente reconhecíveis microfone e câmera fotográfica e de vídeo.");
+            Console.ResetColor();
+            Console.WriteLine("");
+            Console.WriteLine("< Aperte ENTER para continuar >");
+            Console.WriteLine("");
+            Console.ReadKey();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("   Depois de tudo isso, só temos uma pergunta a fazer. Você realmente pensa que a");
+            Console.WriteLine("       física é vista apenas na escola? E que a tecnologia não faz parte dela?");
+            Console.WriteLine("");
+            return null;
+        }
+        public static string final()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("                                                   ╔═╗╦╔╦╗");
+            Console.WriteLine("                                                   ╠╣ ║║║║");
+            Console.WriteLine("                                                   ╚  ╩╩ ╩");
+
+            Console.WriteLine("               Integrantentes do grupo: ");
+            Console.WriteLine("");
+
+            Console.WriteLine("                                         Mel Cristina Pimenta Coli");
+            Console.WriteLine("                                         Giovana dos Santos Tarraque");
+            Console.WriteLine("                                         Cauã Filipi Pessoa Sant' ana");
+            Console.WriteLine("                                         Franklin silva moreira junior");
+            Console.WriteLine("                                         Rodrigo Marinho Meckelburg Peixoto");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("                                    ETERJ - Escola Técnica do Rio de Janeiro");
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            Console.WriteLine("                                                   ╔═╗╦╔╦╗");
+            Console.WriteLine("                                                   ╠╣ ║║║║");
+            Console.WriteLine("                                                   ╚  ╩╩ ╩");
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            titulo();
+            Console.WriteLine("< Aperte ENTER para continuar >");
+            Console.ReadKey();
+            Console.Clear();
+            titulo();
+            Console.WriteLine("");
+            introducao();
+            Console.WriteLine("< Aperte ENTER para continuar >");
+            Console.ReadKey();
+            Console.Clear();
+            titulo();
+            Console.WriteLine("");
+            experiencia1();
+            Console.WriteLine("< Aperte ENTER para continuar >");
+            Console.ReadKey();
+            Console.Clear();
+            titulo();
+            Console.WriteLine("");
+            experiencia2();
+            Console.WriteLine("< Aperte ENTER para continuar >");
+            Console.ReadKey();
+            Console.Clear();
+            titulo();
+            Console.WriteLine("");
+            experiencia3();
+            Console.WriteLine("< Aperte ENTER para continuar >");
+            Console.ReadKey();
+            Console.Clear();
+            titulo();
+            Console.WriteLine("");
+            curiosidade();
+            Console.WriteLine("< Aperte ENTER para continuar >");
+            Console.ReadKey();
+            Console.Clear();
+            final();
+            Console.WriteLine("< Aperte ENTER para finalizar >");
+            Console.ReadKey();
+        }
+    }
+}
